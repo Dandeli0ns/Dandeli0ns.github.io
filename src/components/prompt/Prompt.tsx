3817a6ff28216c9dc93d908c9e0ec84a64b92985
Dandeli0ns <78022174+LucyProctor@users.noreply.github.com>
2024-02-11 22:34:35 +0000

    Things are working!

--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -1,34 +1,57 @@
-import { Button, Label, Select, Textarea, TextInput } from "flowbite-react"
+import { Console } from "console";
+import { Button, Label, Modal, Select, Textarea, TextInput } from "flowbite-react"
 import { Field, Form, Formik } from "formik"
 import OpenAI from "openai";
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 
 export const Prompt = () => {
+    const [imageUrl, setImageUrl] = useState(""); 
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+    
     const openai = new OpenAI({ apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
-
-    const onSubmit = async () => {
-        const response = await openai.images.generate({
-            model: "dall-e-2",
-            prompt: "a white siamese cat",
-            n: 1,
-            size: "1024x1024",
-          });
-    }
 
     return (
         <Formik
-            initialValues={{ model: '', prompt: '', number: '' }}
+            initialValues={{ model: 'dall-e-2', prompt: '', number: '' }}
             onSubmit={async (values, actions) => {
+                setIsSubmitting(true);
+                try{
                 const response = await openai.images.generate({
                     model: values.model,
                     prompt: values.prompt,
-                    n: parseInt(values.number),
+                    n: parseInt(values.number) || 1,
                     size: "1024x1024",
                   });
+
+                  const image = response.data[0].url;
+                  console.log(image);
+                  setImageUrl(image || '');
+                  setIsSubmitting(false);
+                }
+                catch{
+                    setIsSubmitting(false);
+                    setOpenModal(true)
+                }
             }}
             >
             {formik => (
                 <Form>
+                    <Modal show={openModal} onClose={() => setOpenModal(false)}>
+                        <Modal.Header>Oops!</Modal.Header>
+                        <Modal.Body>
+                        <div className="space-y-6">
+                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                An error occured connecting to OpenAI
+                            </p>
+                        </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button color="gray" onClick={() => setOpenModal(false)}>
+                            Decline
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                     <div className="flex flex-col gap-4">
                         <div className="mb-2 block">
                             <Label htmlFor="prompt">Enter image generation prompt</Label>
@@ -49,9 +72,16 @@ export const Prompt = () => {
                             </Field>
                         </div>
                         <div className="mb-4 block">
-                            <Button type="submit">Submit</Button>
+                            <Button type="submit" isProcessing={isSubmitting}>Submit</Button>
                         </div>
                     </div>
+                    {imageUrl && <>
+                    <img src={imageUrl} alt="Generated" width={500}/>
+                    <Button.Group>
+                        <Button color="gray">Download</Button>
+                        <Button color="gray">Regenerate</Button>
+                    </Button.Group>t
+                    </>}
                 </Form>
             )}
         </Formik>
